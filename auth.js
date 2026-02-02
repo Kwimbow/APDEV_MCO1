29edacc66402973701b61168084436472e098442
Kwimbow <kimberly_sotingco@dlsu.edu.ph>
2026-02-02 16:18:30 +0800

    register login done

--- auth.js
+++ auth.js
@@ -1,32 +1,75 @@
 /* JS file that processes the authentication logic for login and registration */
 
 function login(username, password) {
-  if (username === "admin" && password === "1234") {
-    localStorage.setItem("user", JSON.stringify({ username }));
+  let existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+  let validUser = existingUsers.find(user => user.username === username && user.password === password);
+
+  if (validUser){
+    sessionStorage.setItem("user", JSON.stringify({ username }));
     return true;
   }
   return false;
 }
 
+function rememberMe3Weeks(username, password) {
+  let existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+  let validUser = existingUsers.find(user => user.username === username && user.password === password);
+
+  if (validUser){
+    let date = new Date();
+    date.setTime(date.getTime() + (21 * 24 * 60 * 60 * 1000)); //
+    let expires = "; expires=" + date.toUTCString();
+
+    document.cookie = "user=" + JSON.stringify({ username }) + expires + "; path=/";
+    sessionStorage.setItem("user", JSON.stringify({ username }));
+    
+    return true;
+  }
+  return false;
+}
+
+function getCookie(name){
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+
+  if(parts.length === 2){
+    return parts.pop().split(";").shift();
+  }
+}
+
 function logout() {
-    localStorage.clear();
-  //localStorage.removeItem("user");
+    sessionStorage.clear();
+    document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    // localStorage.clear(); //this clears all registered account data
 }
 
 function getCurrentUser() {
-  return JSON.parse(localStorage.getItem("user"));
+  let user = sessionStorage.getItem("user");
+  if (user) {
+    return JSON.parse(user);
+  }
+
+  let cookieUser = getCookie("user");
+  if (cookieUser) {
+    sessionStorage.setItem("user", cookieUser);
+    return JSON.parse(cookieUser);
+  }
+  return null;
 }
 
-//not properly implemented yet
 function register(username, password, repassword) {
-  if (password !== repassword) {
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  //passwords dont match
+  if (password !== repassword){
+    return null;
+  }
+  //there exists a user with the same username
+  if (users.some(user => user.username === username)){
     return false;
   }
-  const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
-  if (existingUsers.find(user => user.username === username)) {
-    return false;
-  }
-  existingUsers.push({ username, password });
-  localStorage.setItem("users", JSON.stringify(existingUsers));
+
+  users.push({ username, password });
+  localStorage.setItem("users", JSON.stringify(users));
   return true;
 }
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /* JS file that contains the shared ui of each navigation button 
    such as the header bar, the side navigation bar, the user's profile, and etc. */  
 
-let btn = document.querySelector("#btn")
-let sideNav = document.querySelector("#side-bar")
-let navItem = document.querySelector("#nav-item")
-let mainContent = document.querySelector("#main-content")
-let home = document.querySelector("#home-btn")
-let bookmark = document.querySelector("#bookmarks-btn")
+let btn = document.querySelector("#btn");
+let sideNav = document.querySelector("#side-bar");
+let navItem = document.querySelector("#nav-item");
+let mainContent = document.querySelector("#main-content");
+let home = document.querySelector("#home-btn");
+let bookmark = document.querySelector("#bookmarks-btn");
 
 var modal = document.getElementById('register-popup');
 var modal2 = document.getElementById('login-popup');
@@ -30,14 +30,17 @@ if (window.location.href.indexOf("index.html") > -1) {
     bookmark.classList.add("active");
 }
 
-
-
 function showPopup(id) {
     document.getElementById(id).style.display = "block";
 }
 
 function hidePopup(id) {
     document.getElementById(id).style.display = "none";
+    if (id === "register-popup"){
+        clearLoginRegister("register-popup");
+    } if (id === "login-popup"){
+        clearLoginRegister("login-popup");
+    }
 }
 
 function setupPopups(){
@@ -54,7 +57,7 @@ function setupPopups(){
     document.querySelectorAll(".modal").forEach(modal => {
         modal.addEventListener("click", (e) => {
             if (e.target === modal) {
-                modal.style.display = "none";
+                hidePopup(modal.id);
             }
         });
     });
@@ -72,21 +75,193 @@ function setupLogout(){
 
 function setupLoginForm(){
     const loginForm = document.querySelector("#login-popup form");
+    var rememberMe = document.querySelector("#remember-3weeks");
 
     if (!loginForm) return;
+
+    let log_user = document.getElementById('log-user-info');
+        let log_pw = document.getElementById('log-pw-info');
+        let log_empty_user = document.getElementById('log-empty-user');
+        let log_empty_pw = document.getElementById('log-empty-pw');
+        let log_unmatch_user_pw = document.getElementById('log-unmatch-user-pw');
+
+        log_user.addEventListener('focus', () => {
+            log_user.classList.remove('wrong');
+            log_pw.classList.remove('wrong');
+            log_empty_user.classList.remove('invalid');
+            log_empty_pw.classList.remove('invalid');
+            log_unmatch_user_pw.classList.remove('invalid');
+        });
+
+        log_pw.addEventListener('focus', () => {
+            log_user.classList.remove('wrong');
+            log_pw.classList.remove('wrong');
+            log_empty_user.classList.remove('invalid');
+            log_empty_pw.classList.remove('invalid');
+            log_unmatch_user_pw.classList.remove('invalid');
+        });
 
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const username = loginForm.querySelector('input[name="uname"]').value;
-        const password = loginForm.querySelector('input[name="psw"]').value;
+        let username = loginForm.querySelector('input[name="uname"]').value;
+        let password = loginForm.querySelector('input[name="psw"]').value;
+        let successful = false;
 
-        if (login(username, password)) {
-            hidePopup("login-popup");
-            updateUserUI();
-        } else {
-            alert("Try admin / 1234");
+        username = username.trim();
+        password = password.trim();
+
+        if(username === "" || password === ""){
+            if(username === ""){
+                errorShake(log_user, log_empty_user);
+                log_user.classList.add('wrong');
+                log_empty_user.classList.add('invalid');
+                log_unmatch_user_pw.classList.remove('invalid');
+            } if(password === ""){
+                errorShake(log_pw, log_empty_pw);
+                log_pw.classList.add('wrong');
+                log_empty_pw.classList.add('invalid');
+                log_unmatch_user_pw.classList.remove('invalid');
+            }
+        }else{
+            if(rememberMe.checked){
+                successful = rememberMe3Weeks(username, password);
+            } else{
+                successful = login(username, password);
+            }
+
+            if (successful) {
+                hidePopup("login-popup");
+                updateUserUI();
+            } else {
+                errorShake(log_user, log_pw, log_unmatch_user_pw);
+                log_user.classList.add('wrong');
+                log_pw.classList.add('wrong');
+                log_unmatch_user_pw.classList.add('invalid');
+                log_empty_pw.classList.remove('invalid');
+            }
         }
+
+        
+    });
+}
+
+function setupRegisterForm(){
+    const registerForm = document.querySelector("#register-popup form");
+
+    if (!registerForm) return;
+
+    let reg_user = document.getElementById('reg-user-info');
+        let reg_pw = document.getElementById('reg-pw-info');
+        let reg_repw = document.getElementById('reg-repw-info');
+        let reg_empty_user = document.getElementById('reg-empty-user');
+        let reg_taken_user = document.getElementById('reg-taken-user');
+        let reg_empty_pw = document.getElementById('reg-empty-pw');
+        let reg_unmatch_pw = document.getElementById('reg-unmatch-pw');
+
+        reg_user.addEventListener('focus', () => {
+            reg_user.classList.remove('wrong');
+            reg_empty_user.classList.remove('invalid');
+            reg_taken_user.classList.remove('invalid');
+            reg_pw.classList.remove('wrong');
+            reg_repw.classList.remove('wrong');
+            reg_empty_pw.classList.remove('invalid');
+            reg_unmatch_pw.classList.remove('invalid');
+        });
+
+        reg_pw.addEventListener('focus', () => {
+            reg_pw.classList.remove('wrong');
+            reg_repw.classList.remove('wrong');
+            reg_empty_pw.classList.remove('invalid');
+            reg_unmatch_pw.classList.remove('invalid');
+        });
+
+        reg_repw.addEventListener('focus', () => {
+            reg_pw.classList.remove('wrong');
+            reg_repw.classList.remove('wrong');
+            reg_empty_pw.classList.remove('invalid');
+            reg_unmatch_pw.classList.remove('invalid');
+        });
+
+    registerForm.addEventListener("submit", (e) =>{
+        e.preventDefault();
+
+        let username = registerForm.querySelector('input[name="uname"]').value;
+        let password = registerForm.querySelector('input[name="psw"]').value;
+        let repassword = registerForm.querySelector('input[name="repsw"]').value;
+
+        username = username.trim();
+        password = password.trim();   
+        repassword = repassword.trim();
+        
+        const result = register(username, password, repassword);
+
+        if(username === "" || password === "" || repassword === ""){
+            if(username === ""){
+                errorShake(reg_user, reg_empty_user);
+                reg_user.classList.add('wrong');
+                reg_empty_user.classList.add('invalid');
+                reg_taken_user.classList.remove('invalid');
+            }if(password === "" || repassword === ""){
+                reg_empty_pw.classList.add('invalid');
+                if(password === ""){
+                    errorShake(reg_pw, reg_empty_pw);
+                    reg_pw.classList.add('wrong');
+                }if(repassword === ""){
+                    errorShake(reg_repw, reg_empty_pw);
+                    reg_repw.classList.add('wrong');
+                }
+                reg_unmatch_pw.classList.remove('invalid');
+            }
+        }else{
+            if(result === true){
+                hidePopup("register-popup");
+                successfullyRegMsg();
+            } else if(result === null){
+                //show red on both password and display message: passwords do not match
+                errorShake(reg_pw, reg_repw, reg_unmatch_pw);
+                reg_pw.classList.add('wrong');
+                reg_repw.classList.add('wrong');
+                reg_unmatch_pw.classList.add('invalid');
+                reg_empty_pw.classList.remove('invalid');
+            } else if(result === false){
+                //display username is taken
+                errorShake(reg_user, reg_taken_user);
+                reg_user.classList.add('wrong');
+                reg_taken_user.classList.add('invalid');
+                reg_empty_user.classList.remove('invalid');
+            }
+        }
+    });
+}
+
+function errorShake(...elements) {
+    elements.forEach(el => {
+        el.classList.remove('error-shake');
+        void el.offsetWidth;
+        el.classList.add('error-shake');
+    });
+}
+
+function successfullyRegMsg(){
+    const msg = document.getElementById("reg-success");
+    msg.classList.add("show");
+    setTimeout(() => {
+        msg.classList.remove("show");
+    }, 3000);
+}
+
+function clearLoginRegister(formId){
+    let input = document.querySelector(`#${formId} form`);
+
+    if (!input) return;
+
+    input.querySelectorAll("input").forEach(data => {
+        data.value = "";
+        data.classList.remove("wrong");
+    });
+    input.querySelectorAll(".invalid").forEach(msg => {
+        msg.classList.remove("invalid");
     });
 }
 
@@ -136,6 +311,7 @@ function updateUserUI() {
 document.addEventListener("DOMContentLoaded", () => {
     setupPopups();
     setupLoginForm();
+    setupRegisterForm();
     setupLogout();
     updateUserUI();
     showBookmarksBar();
