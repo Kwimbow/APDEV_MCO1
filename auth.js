8d6af5d10177d8d367a2f4424c801c46bf2bf82f
Kwimbow <kimberly_sotingco@dlsu.edu.ph>
2026-02-02 18:30:16 +0800

    added comments and fixed modal background to stop moving or access body

--- auth.js
+++ auth.js
@@ -1,5 +1,8 @@
 /* JS file that processes the authentication logic for login and registration */
 
+/* This function searches the users stored in local storage and logs in using session storage (temporary)
+RETURN: true if username & password matches one in the storage
+        false if either of them don't match */
 function login(username, password) {
   let existingUsers = JSON.parse(localStorage.getItem("users")) || [];
   let validUser = existingUsers.find(user => user.username === username && user.password === password);
@@ -11,6 +14,9 @@ function login(username, password) {
   return false;
 }
 
+/* This function is accessed when the user clicks the remember me button. Remembers user for 3 weeks
+RETURN: true if username & password matches one in the storage
+        false if either of them don't match */
 function rememberMe3Weeks(username, password) {
   let existingUsers = JSON.parse(localStorage.getItem("users")) || [];
   let validUser = existingUsers.find(user => user.username === username && user.password === password);
@@ -28,6 +34,7 @@ function rememberMe3Weeks(username, password) {
   return false;
 }
 
+/* This function returns the value of cookie, yummy */
 function getCookie(name){
   let value = "; " + document.cookie;
   let parts = value.split("; " + name + "=");
@@ -37,12 +44,16 @@ function getCookie(name){
   }
 }
 
+/* This function is accessed when user logs out, terminates logged in period */
 function logout() {
     sessionStorage.clear();
     document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     // localStorage.clear(); //this clears all registered account data
 }
 
+/* This function gets the current user and checks if they are logged in or not (visitor)
+RETURN: JS object or a user value if current user is logged in,
+        null if current user is a visitor */
 function getCurrentUser() {
   let user = sessionStorage.getItem("user");
   if (user) {
@@ -57,6 +68,10 @@ function getCurrentUser() {
   return null;
 }
 
+/* This function registers the user
+RETURN: true if successfully registered and stored in local storage
+        false if username already exists in local storage
+        null if password does not match the re-entered password's  */
 function register(username, password, repassword) {
   let users = JSON.parse(localStorage.getItem("users")) || [];
 
--- main.js
+++ main.js
@@ -11,7 +11,10 @@ let bookmark = document.querySelector("#bookmarks-btn");
 var modal = document.getElementById('register-popup');
 var modal2 = document.getElementById('login-popup');
 
-// Toggle Side Navigation
+/* This function controls the display of the side navigation buttons,
+when btn(menu button) is clicked, it minimizes the side bar, the navigation buttons,
+the btn(menu button) movement to sabay with the side bar's minimization, and expands
+the main page for a wider view */
 btn.onclick = function() {
     // Minimize the side navigation white box
     sideNav.classList.toggle("minimize");
@@ -23,19 +26,27 @@ btn.onclick = function() {
     mainContent.classList.toggle("expand");
 }
 
-// Highlight Active Page
+/* Brute forced active navigation button to indicate which page the user is in */
 if (window.location.href.indexOf("index.html") > -1) {
     home.classList.add("active");
 } else if (window.location.href.indexOf("bookmarks.html") > -1) {
     bookmark.classList.add("active");
 }
 
+/* This function shows the pop-up of the register and login "page", 
+it also locks the body's content so that it cannot be scrolled */
 function showPopup(id) {
     document.getElementById(id).style.display = "block";
+    document.body.classList.add("modal-open");
 }
 
+/* This function hides the pop-up of the register and login "page", 
+it also unlocks the body's content so that it can be scrolled/accessible when
+this "page" closes */
 function hidePopup(id) {
     document.getElementById(id).style.display = "none";
+    document.body.classList.remove("modal-open");
+
     if (id === "register-popup"){
         clearLoginRegister("register-popup");
     } if (id === "login-popup"){
@@ -43,17 +54,19 @@ function hidePopup(id) {
     }
 }
 
+/* This function handles the register and login button on the upper right corner,
+when "register" button is clicked, open/show register pop-up ui,
+when "login" button is clicked, open/show login pop-up ui.
+Also handles the background clicks, so if user clicks pop-up background,
+the pop-up of either register or login should be hidden */
 function setupPopups(){
-  // Open buttons
     document.getElementById("reg")?.addEventListener("click", () => {
         showPopup("register-popup");
     });
-
     document.getElementById("log")?.addEventListener("click", () => {
         showPopup("login-popup");
     });
 
-    // Click outside to close
     document.querySelectorAll(".modal").forEach(modal => {
         modal.addEventListener("click", (e) => {
             if (e.target === modal) {
@@ -63,6 +76,8 @@ function setupPopups(){
     });
 }
 
+/* This function handles the logging out of the user,
+TO BE EDITED ONCE THE SETTINGS POPOUT IS MADE */
 function setupLogout(){
     const logoutBtn = document.getElementById("settings-logged-in");//<-- change this to log out when actual button is made
 
@@ -73,6 +88,12 @@ function setupLogout(){
     });
 }
 
+/* This function handles the data inputted in the log in pop-up,
+currently handles "errors" and shows animations (shaky red + error msg) when it comes into 
+contact with "errors". Handles "error" msgs to show one at a time.
+
+ Known "Errors" are: Empty username field, Empty password field, and when
+                   Account inputted is not in "server" (non-registered account)*/
 function setupLoginForm(){
     const loginForm = document.querySelector("#login-popup form");
     var rememberMe = document.querySelector("#remember-3weeks");
@@ -80,37 +101,43 @@ function setupLoginForm(){
     if (!loginForm) return;
 
     let log_user = document.getElementById('log-user-info');
-        let log_pw = document.getElementById('log-pw-info');
-        let log_empty_user = document.getElementById('log-empty-user');
-        let log_empty_pw = document.getElementById('log-empty-pw');
-        let log_unmatch_user_pw = document.getElementById('log-unmatch-user-pw');
+    let log_pw = document.getElementById('log-pw-info');
+    let log_empty_user = document.getElementById('log-empty-user');
+    let log_empty_pw = document.getElementById('log-empty-pw');
+    let log_unmatch_user_pw = document.getElementById('log-unmatch-user-pw');
 
-        log_user.addEventListener('focus', () => {
-            log_user.classList.remove('wrong');
-            log_pw.classList.remove('wrong');
-            log_empty_user.classList.remove('invalid');
-            log_empty_pw.classList.remove('invalid');
-            log_unmatch_user_pw.classList.remove('invalid');
-        });
+    //if username input field is clicked, remove the "errors"
+    log_user.addEventListener('focus', () => {
+        log_user.classList.remove('wrong');
+        log_pw.classList.remove('wrong');
+        log_empty_user.classList.remove('invalid');
+        log_empty_pw.classList.remove('invalid');
+        log_unmatch_user_pw.classList.remove('invalid');
+    });
 
-        log_pw.addEventListener('focus', () => {
-            log_user.classList.remove('wrong');
-            log_pw.classList.remove('wrong');
-            log_empty_user.classList.remove('invalid');
-            log_empty_pw.classList.remove('invalid');
-            log_unmatch_user_pw.classList.remove('invalid');
-        });
+    //if password input field is clicked, remove the "errors"
+    log_pw.addEventListener('focus', () => {
+        log_user.classList.remove('wrong');
+        log_pw.classList.remove('wrong');
+        log_empty_user.classList.remove('invalid');
+        log_empty_pw.classList.remove('invalid');
+        log_unmatch_user_pw.classList.remove('invalid');
+    });
 
+    //when user submits log in details
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
+        //get the inputted values
         let username = loginForm.querySelector('input[name="uname"]').value;
         let password = loginForm.querySelector('input[name="psw"]').value;
-        let successful = false;
+        let successful;
 
+        //trim the inputted values to prevent leading & trailing spaces
         username = username.trim();
         password = password.trim();
 
+        //if any input is empty show its specific "error" animations + msges
         if(username === "" || password === ""){
             if(username === ""){
                 errorShake(log_user, log_empty_user);
@@ -123,16 +150,21 @@ function setupLoginForm(){
                 log_empty_pw.classList.add('invalid');
                 log_unmatch_user_pw.classList.remove('invalid');
             }
+        //if there are no empty inputs
         }else{
+            //verify if remember me button is toggled
             if(rememberMe.checked){
                 successful = rememberMe3Weeks(username, password);
+            //otherwise, only log the user in for that session, close website -> account logged out
             } else{
                 successful = login(username, password);
             }
 
+            //if successfully logged in, update user's UI
             if (successful) {
                 hidePopup("login-popup");
                 updateUserUI();
+            //otherwise, not successfully logged in because of wrong credentials or account not registered
             } else {
                 errorShake(log_user, log_pw, log_unmatch_user_pw);
                 log_user.classList.add('wrong');
@@ -146,43 +178,54 @@ function setupLoginForm(){
     });
 }
 
+/* This function handles the data inputted in the register pop-up,
+currently handles "errors" and shows animations (shaky red + error msg) when it comes into 
+contact with "errors". Handles "error" msgs to show one at a time.
+
+ Known "Errors" are: Empty username field, Empty password/re-enter password field,
+                   Password and Re-entered Password don't match, and Taken username
+
+What currently isn't handled yet is the minimum password length. */
 function setupRegisterForm(){
     const registerForm = document.querySelector("#register-popup form");
 
     if (!registerForm) return;
 
     let reg_user = document.getElementById('reg-user-info');
-        let reg_pw = document.getElementById('reg-pw-info');
-        let reg_repw = document.getElementById('reg-repw-info');
-        let reg_empty_user = document.getElementById('reg-empty-user');
-        let reg_taken_user = document.getElementById('reg-taken-user');
-        let reg_empty_pw = document.getElementById('reg-empty-pw');
-        let reg_unmatch_pw = document.getElementById('reg-unmatch-pw');
+    let reg_pw = document.getElementById('reg-pw-info');
+    let reg_repw = document.getElementById('reg-repw-info');
+    let reg_empty_user = document.getElementById('reg-empty-user');
+    let reg_taken_user = document.getElementById('reg-taken-user');
+    let reg_empty_pw = document.getElementById('reg-empty-pw');
+    let reg_unmatch_pw = document.getElementById('reg-unmatch-pw');
 
-        reg_user.addEventListener('focus', () => {
-            reg_user.classList.remove('wrong');
-            reg_empty_user.classList.remove('invalid');
-            reg_taken_user.classList.remove('invalid');
-            reg_pw.classList.remove('wrong');
-            reg_repw.classList.remove('wrong');
-            reg_empty_pw.classList.remove('invalid');
-            reg_unmatch_pw.classList.remove('invalid');
-        });
+    //if username input is clicked, remove all "errors"
+    reg_user.addEventListener('focus', () => {
+        reg_user.classList.remove('wrong');
+        reg_empty_user.classList.remove('invalid');
+        reg_taken_user.classList.remove('invalid');
+        reg_pw.classList.remove('wrong');
+        reg_repw.classList.remove('wrong');
+        reg_empty_pw.classList.remove('invalid');
+        reg_unmatch_pw.classList.remove('invalid');
+    });
 
-        reg_pw.addEventListener('focus', () => {
-            reg_pw.classList.remove('wrong');
-            reg_repw.classList.remove('wrong');
-            reg_empty_pw.classList.remove('invalid');
-            reg_unmatch_pw.classList.remove('invalid');
-        });
+    //if password / re-enter pw input is clicked, remove all "errors" except taken username
+    reg_pw.addEventListener('focus', () => {
+        reg_pw.classList.remove('wrong');
+        reg_repw.classList.remove('wrong');
+        reg_empty_pw.classList.remove('invalid');
+        reg_unmatch_pw.classList.remove('invalid');
+    });
 
-        reg_repw.addEventListener('focus', () => {
-            reg_pw.classList.remove('wrong');
-            reg_repw.classList.remove('wrong');
-            reg_empty_pw.classList.remove('invalid');
-            reg_unmatch_pw.classList.remove('invalid');
-        });
+    reg_repw.addEventListener('focus', () => {
+        reg_pw.classList.remove('wrong');
+        reg_repw.classList.remove('wrong');
+        reg_empty_pw.classList.remove('invalid');
+        reg_unmatch_pw.classList.remove('invalid');
+    });
 
+    //when user submits register details
     registerForm.addEventListener("submit", (e) =>{
         e.preventDefault();
 
@@ -190,20 +233,26 @@ function setupRegisterForm(){
         let password = registerForm.querySelector('input[name="psw"]').value;
         let repassword = registerForm.querySelector('input[name="repsw"]').value;
 
+        //trim all to prevent leading/trailing white spaces
         username = username.trim();
         password = password.trim();   
         repassword = repassword.trim();
         
+        //call the register function from auth.js to verify the inputs
         const result = register(username, password, repassword);
 
+        //if any input field is empty show its animations and "error" msges
         if(username === "" || password === "" || repassword === ""){
             if(username === ""){
                 errorShake(reg_user, reg_empty_user);
                 reg_user.classList.add('wrong');
                 reg_empty_user.classList.add('invalid');
                 reg_taken_user.classList.remove('invalid');
+
+            //if either passwords has empty fields, show pw cannot be empty
             }if(password === "" || repassword === ""){
                 reg_empty_pw.classList.add('invalid');
+                //show their respective "errors"
                 if(password === ""){
                     errorShake(reg_pw, reg_empty_pw);
                     reg_pw.classList.add('wrong');
@@ -214,9 +263,12 @@ function setupRegisterForm(){
                 reg_unmatch_pw.classList.remove('invalid');
             }
         }else{
+            //registers the user's account, allowing them to log in manually, show successful msg
             if(result === true){
                 hidePopup("register-popup");
                 successfullyRegMsg();
+
+            //function returns null if passwords dont match
             } else if(result === null){
                 //show red on both password and display message: passwords do not match
                 errorShake(reg_pw, reg_repw, reg_unmatch_pw);
@@ -224,6 +276,8 @@ function setupRegisterForm(){
                 reg_repw.classList.add('wrong');
                 reg_unmatch_pw.classList.add('invalid');
                 reg_empty_pw.classList.remove('invalid');
+
+            //function returns false if the username has beed taken by another account
             } else if(result === false){
                 //display username is taken
                 errorShake(reg_user, reg_taken_user);
@@ -235,6 +289,8 @@ function setupRegisterForm(){
     });
 }
 
+/* This function "redoes" the error shaking everytime the user clicks log-in or register
+submit button */
 function errorShake(...elements) {
     elements.forEach(el => {
         el.classList.remove('error-shake');
@@ -243,6 +299,7 @@ function errorShake(...elements) {
     });
 }
 
+/* This function displays a slide up message "successfully registered" for 3 seconds */
 function successfullyRegMsg(){
     const msg = document.getElementById("reg-success");
     msg.classList.add("show");
@@ -251,6 +308,8 @@ function successfullyRegMsg(){
     }, 3000);
 }
 
+/* This function clears the log in / register input form so that when the user
+clicks out, their data that is in the input fields do not save when button is clicked again */
 function clearLoginRegister(formId){
     let input = document.querySelector(`#${formId} form`);
 
@@ -265,12 +324,17 @@ function clearLoginRegister(formId){
     });
 }
 
+/* This function redirects the user back to the home page whenever they log out,
+just in case the user logs out of a page that is inaccessible to visitors / non-logged in users */
 function logoutAndRedirect(){
     logout();
     updateUserUI();
     window.location.href = "index.html";
 }
 
+/* This function unhides the bookmarks navigation button if the user is logged in.
+NOTE: If you want to make more navigation bar buttons, duplicate this function, rename
+      what is required to be renamed and add to updateUserUI() */
 function showBookmarksBar(){
     if(getCurrentUser() !== null){
         document.getElementById("bookmarks-bar").style.display = "block";
@@ -279,6 +343,8 @@ function showBookmarksBar(){
     }
 }
 
+/* This function prohibits visitors from accessing the bookmarks page, even if they
+change the link name to bookmarks.html */
 function protectBookmarksPage(){
     if (window.location.href.includes("bookmarks.html")) {
         if (getCurrentUser() === null) {
@@ -287,6 +353,7 @@ function protectBookmarksPage(){
     }
 }
 
+/* This function dynamically updates the user view as soon as they log in */
 function updateUserUI() {
     const user = getCurrentUser();
     const postBtn = document.getElementById("create-post-login");
@@ -295,8 +362,13 @@ function updateUserUI() {
 
     if (!postBtn || !miniProfile || !regLoginGroup) return;
 
+    //add more views here (profile, settings, create post pop-up, and many more)
     showBookmarksBar();
 
+    //end of views
+
+    //when logged in, displays the create post button, mini profile button (upper right corner), 
+    // and hides the register/login button
     if (user !== null) {
         postBtn.style.display = "block";
         miniProfile.style.display = "block";
@@ -308,12 +380,12 @@ function updateUserUI() {
     }
 }
 
+//calls the functions
 document.addEventListener("DOMContentLoaded", () => {
     setupPopups();
     setupLoginForm();
     setupRegisterForm();
     setupLogout();
     updateUserUI();
-    showBookmarksBar();
     protectBookmarksPage();
 });
