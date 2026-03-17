8b640a54edff6473539f72f9ab44d969535b0375
Koby <jacobaluna8@gmail.com>
2026-03-17 11:30:05 +0800

    added pfp to schem

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true },
 	// email: { type: String, required: true, unique: true },
 	bio: { type: String },
+	pfp: { type: String },
 	password: { type: String, required: true },
 	createdAt: {type: Date, required: true, default: () => Date.now() },
 	cupcakes: { type: Number, required: true, default: 0 }
