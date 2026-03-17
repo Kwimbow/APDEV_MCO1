ae688054cb5eefa09c699bdf453d195033a6abfd
ena0553 <enriquemiguelaragon@gmail.com>
2026-03-17 22:31:04 +0800

    adding auth post stuff

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,8 @@ const postSchema = new mongoose.Schema({
 	title: { type: String, required: true },
 	content: { type: String, required: true },
 	createdAt: { type: Date, required: true, default: () => Date.now() },
-	upvotes: { type: Number, required: true, default: 0 }
+	tag: { type: String, required: true },
+	upvotes: { type: Number, required: true, default: 0 }, 
 });
 
 module.exports = mongoose.model('Post', postSchema);
---
+++ routes/authpost.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const User = require('../models/Post');
+
+/* This function registers the user.
+    Can return an error if there is another user with the inputted username.*/
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const notUnique = await User.findOne({ username });
+    if (notUnique) {
+        return res.status(400).json({message: 'Username already exists' });
+    }
+
+    await User.create({ username, password });
+    res.json({ success: true });
+});
+
+/* This function logs in the user. */
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username, password });
+    if (!user) {
+        return res.status(400).json({ message: "Invalid username or password "});
+    }
+
+    res.json({ success: true, user: { username } });
+});
+
+module.exports = router;
