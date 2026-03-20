0006e4652762577ad440cd9af7ae2b98ef096fa6
kekekoby <jacobaluna8@gmail.com>
2026-03-20 13:27:47 +0800

    feet: made router functions for comments

--- routes/authcomments.js
+++ routes/authcomments.js
@@ -5,10 +5,32 @@ const Comment = require('../models/Comment');
     Can return an error if there is another user with the inputted username.*/
 router.post('/', async (req, res) => {
     const { comment } = req.body;
+    const newComment = await Comment.create(comment);
+    const populated = await newComment.populate('author', 'username');
+    res.json({ success: true, comment: populated });
+});
 
-    await Comment.create(comment);
+// gets all comments for a post
+router.get('/:postId', async (req, res) => {
+    const comments = await Comment.find({ post: req.params.postId })
+        .populate('author', 'username');
+    res.json(comments);
+});
+
+// edit a comment
+router.patch('/:commentId', async (req, res) => {
+    const { content } = req.body;
+    await Comment.findByIdAndUpdate(req.params.commentId, {
+        content,
+        edited: true
+    });
     res.json({ success: true });
 });
 
+// delete a comment
+router.delete('/:commentId', async (req, res) => {
+    await Comment.findByIdAndUpdate(req.params.commentId, { deleted: true });
+    res.json({ success: true });
+});
 
 module.exports = router;
