0e0862f8dd29e78c48f82104949323bbc1a74fc2
kekekoby <jacobaluna8@gmail.com>
2026-04-07 13:05:48 +0800

    file changes for deployment

---
+++ db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error('Please define MONGO_URI in your environment variables');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = connectDB;
